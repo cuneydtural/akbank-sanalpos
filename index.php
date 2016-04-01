55a8c7eb0058c35c81702db7cb979553f530fa73
Cüneyd Tural <turalcuneyd@gmail.com>
2016-04-01 14:40:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,22 +5,48 @@
  * Date: 31.03.2016
  * Time: 14:48
  */
-
 require 'vendor/autoload.php';
-
 use Virtualpos\Akbank;
 
+/* Mağaza Kodu, Kullanıcı Adı, Şifre */
 $akbank = new Akbank('100100000', 'AKTESTAPI', 'AKBANK01');
+
+/* Sipariş Numarası  */
 $akbank->setOrder('9898989898989');
+
+/* Para Birimi  */
 $akbank->setCurrency('949');
+
+/* Kredi Kartı Numarası  */
 $akbank->setPan('4938410114062912');
-$akbank->setCvv('956');
+
+/* 3 Haneli güvenlik numarası  */
+$akbank->setCvv('956'); //
+
+/* Son Kullanma Tarihi  */
 $akbank->setExpiry('2001');
+
+/* Çekilecek Tutar  */
 $akbank->setTotal('50.00');
+
+/* Taksit  */
 $akbank->setInstalment('0');
+
+/* Provizyon  */
 $akbank->setType('Auth');
+
+/* XML formatına hazırla  */
 $akbank->setXml();
-$akbank->getXml();
+
+/* Bankaya Gönder  */
 $akbank->send();
+
+
+/* Gelen Sonuca göre işlem yap  */
+if ($akbank->return_code == '00') {
+    echo 'ödeme başarılı';
+} else {
+    echo 'ödeme başarısız. (' . $akbank->error_message . ')';
+}
 
 
